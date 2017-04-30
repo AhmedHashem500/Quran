7122a4c7fd0dd7b67664af13564a9348fbd22c23
ahmedayman2011996 <kingfire1996@gmail.com>
2017-04-30 19:28:18 +0200

    Add files via upload

---
+++ script/012.js
@@ -0,0 +1,99 @@
+var counter =1;
+                function next(){
+					"use strict";
+					counter++;
+				    if(counter == 2){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img2.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+                    else if(counter == 3){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img3.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 4){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img4.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 5){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img5.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 6){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img6.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 7){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img7.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 8){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img8.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 9){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img9.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 10){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img10.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 11){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img11.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 12){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img12.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 13){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img13.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 else if(counter == 14){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img14.jpg");
+                       //txt
+					document.getElementById("txt").innerHTML='';}
+					 
+				else {counter = 14;}
+					
+            } 
+                
+                function previous(){
+                    counter--;
+                    if(counter == 1){
+                        //img
+                    var img=document.getElementById("img");
+                    img.setAttribute("src","../pics/012/img1.jpg");
+                        //txt
+                    document.getElementById("txt").innerHTML='<b style="color: lightblue">البقرة</b><br> <mark>{1} الم</mark><br>الم" اللَّه أَعْلَم بِمُرَادِهِ بِذَلِكَ .<br><br> <mark>{2} ذَلِكَ الْكِتَابُ لَا رَيْبَ فِيهِ هُدًى لِلْمُتَّقِينَ</mark><br>"ذَلِك" أَيْ هَذَا "الْكِتَاب" الَّذِي يَقْرَؤُهُ مُحَمَّد . "لَا رَيْب" لَا شَكَّ "فِيهِ" أَنَّهُ مِنْ عِنْد اللَّه وَجُمْلَة النَّفْي خَبَر مُبْتَدَؤُهُ ذَلِك وَالْإِشَارَة بِهِ لِلتَّعْظِيمِ "هُدًى" خَبَر ثَانٍ أَيْ هَادٍ "لِلْمُتَّقِينَ" الصَّائِرِينَ إلَى التَّقْوَى بِامْتِثَالِ الْأَوَامِر وَاجْتِنَاب النَّوَاهِي لِاتِّقَائِهِمْ بِذَلِكَ النَّار <br><br> <mark>{3} الَّذِينَ يُؤْمِنُونَ بِالْغَيْبِ وَيُقِيمُونَ الصَّلَاةَ وَمِمَّا رَزَقْنَاهُمْ يُنْفِقُونَ</mark><br>"الَّذِينَ يُؤْمِنُونَ" يُصَدِّقُونَ "بِالْغَيْبِ" بِمَا غَابَ عَنْهُمْ مِنْ الْبَعْث وَالْجَنَّة وَالنَّار "وَيُقِيمُونَ الصَّلَاة" أَيْ يَأْتُونَ بِهَا بِحُقُوقِهَا "وَمِمَّا رَزَقْنَاهُمْ" أَعْطَيْنَاهُمْ "يُنْفِقُونَ" فِي طَاعَة اللَّه <br><br> <mark>{4} وَالَّذِينَ يُؤْمِنُونَ بِمَا أُنْزِلَ إِلَيْكَ وَمَا أُنْزِلَ مِنْ قَبْلِكَ وَبِالْآخِرَةِ هُمْ يُوقِنُونَ</mark><br>"وَاَلَّذِينَ يُؤْمِنُونَ بِمَا أُنْزِلَ إلَيْك" أَيْ الْقُرْآن "وَمَا أُنْزِلَ مِنْ قَبْلك" أَيْ التَّوْرَاة وَالْإِنْجِيل وَغَيْرهمَا "وَبِالْآخِرَةِ هُمْ يُوقِنُونَ" يَعْلَمُونَ <br><br> <mark>{5} أُولَئِكَ عَلَى هُدًى مِنْ رَبِّهِمْ وَأُولَئِكَ هُمُ الْمُفْلِحُونَ</mark><br>"أُولَئِكَ" الْمَوْصُوفُونَ بِمَا ذُكِرَ "عَلَى هُدًى مِنْ رَبّهمْ وَأُولَئِكَ هُمْ الْمُفْلِحُونَ" الْفَائِزُونَ بِالْجَنَّةِ النَّاجُونَ مِنْ النَّار<br><br>';
+                    }
+                   
+                    else {counter = 1;}
+            } 
